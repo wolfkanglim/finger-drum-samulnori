e656d03be2d0eb027f523eb7c722b4b3fe0861ee
truckDriverDev <hungrywolfkang@gmail.com>
2022-03-14 18:25:40 -0400

    add playSound()
    if (audioCtx.state === "suspended") {
        audioCtx.resume();
        audioCtx.state = "running";
      }

--- script.js
+++ script.js
@@ -48,6 +48,10 @@ pads.forEach((pad) => {
 })
 var MEDIA_ELEMENT_NODES = new WeakMap();
 function playSound(pad){
+    if (audioCtx.state === "suspended") {
+    audioCtx.resume();
+    audioCtx.state = "running";
+  }
     let padSound = document.getElementById(pad.dataset.sound);
     padSound.crossOrigin = "anonymous";
     padSound.currentTime = 0;
