91669c47e2b05eb447f3336b878f35e3479b932c
truckDriverDev <hungrywolfkang@gmail.com>
2022-03-15 06:52:44 -0400

    vol 0.12

--- script.js
+++ script.js
@@ -67,7 +67,7 @@ function playSound(pad){
           }
     let padSound = document.getElementById(pad.dataset.sound);
     padSound.currentTime = 0;
-    padSound.volume = 0.2;
+    padSound.volume = 0.12;
     padSound.play();
     pad.classList.add('playing');
     pad.addEventListener('mouseup', () => {
