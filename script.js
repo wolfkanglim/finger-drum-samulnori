e670aacc0efccf0cccebe57e4a374b6c9e22253e
truckDriverDev <hungrywolfkang@gmail.com>
2022-03-16 07:11:45 -0400

    add unlock

--- script.js
+++ script.js
@@ -28,15 +28,15 @@ const trebEQ = new BiquadFilterNode(audioCtx, {
     gain: treb.value
 })
 
-/////to fix unlock
-// function unlockAudioContext(audioCtx) {
-//   if (audioCtx.state !== 'suspended') return;
-//   const b = document.body;
-//   const events = ['touchstart','touchend', 'mousedown','keydown'];
-//   events.forEach(e => b.addEventListener(e, unlock, false));
-//   function unlock() { audioCtx.resume().then(clean); }
-//   function clean() { events.forEach(e => b.removeEventListener(e, unlock)); }
-// }
+///to fix unlock
+function unlockAudioContext(audioCtx) {
+  if (audioCtx.state !== 'suspended') return;
+  const b = document.body;
+  const events = ['touchstart','touchend', 'mousedown','keydown'];
+  events.forEach(e => b.addEventListener(e, unlock, false));
+  function unlock() { audioCtx.resume().then(clean); }
+  function clean() { events.forEach(e => b.removeEventListener(e, unlock)); }
+}
 
 /////Images and Texts/////
 function drawImages(){
