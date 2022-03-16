caf52da87df6dc4d8f50c7125a52951790dc6d06
truckDriverDev <hungrywolfkang@gmail.com>
2022-03-16 07:34:42 -0400

    volume set 0.5

--- script.js
+++ script.js
@@ -68,7 +68,7 @@ function playSound(pad){
           }
     let padSound = document.getElementById(pad.dataset.sound);
     padSound.currentTime = 0;
-    padSound.volume = 0.2;
+    padSound.volume = 0.5;
     padSound.play();
     pad.classList.add('playing');
     pad.addEventListener('mouseup', () => {
