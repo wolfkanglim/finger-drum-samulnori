c2a74a9996808329dc128ed63f160817e420271e
truckDriverDev <hungrywolfkang@gmail.com>
2022-03-16 07:00:09 -0400

    commented out unlock

--- script.js
+++ script.js
@@ -29,14 +29,14 @@ const trebEQ = new BiquadFilterNode(audioCtx, {
 })
 
 /////to fix unlock
-function unlockAudioContext(audioCtx) {
-  if (audioCtx.state !== 'suspended') return;
-  const b = document.body;
-  const events = ['touchstart','touchend', 'mousedown','keydown'];
-  events.forEach(e => b.addEventListener(e, unlock, false));
-  function unlock() { audioCtx.resume().then(clean); }
-  function clean() { events.forEach(e => b.removeEventListener(e, unlock)); }
-}
+// function unlockAudioContext(audioCtx) {
+//   if (audioCtx.state !== 'suspended') return;
+//   const b = document.body;
+//   const events = ['touchstart','touchend', 'mousedown','keydown'];
+//   events.forEach(e => b.addEventListener(e, unlock, false));
+//   function unlock() { audioCtx.resume().then(clean); }
+//   function clean() { events.forEach(e => b.removeEventListener(e, unlock)); }
+// }
 
 /////Images and Texts/////
 function drawImages(){
