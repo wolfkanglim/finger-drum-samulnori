cf759ee4d9846dd63674b917d5ce75bf1a168691
truckDriverDev <hungrywolfkang@gmail.com>
2022-03-23 01:03:10 -0400

    Update script.js

--- script.js
+++ script.js
@@ -60,7 +60,7 @@ function setViewportHeight() {
     let vh = window.innerHeight * 0.01;
     document.documentElement.style.setProperty('--vh', `${vh}px`);
 }
-setVierportHeight();
+setViewportHeight();
 window.addEventListener('resize', () => {
     setTimeout(setViewportHeight, 100)
 })
